70e6459cf03cf50d4924a7b2e47c58e629435f36
C-Beck <C-Beck@users.noreply.github.com>
2016-06-03 12:32:50 +0000

    Created by Coding Ground Client

---
+++ New Project-20160603/GUITest.java
@@ -0,0 +1,12 @@
+import java.awt.Frame;
+
+public class GUITest extends Frame{
+    public GUITest(){
+        
+        
+    }
+
+     public static void main(String []args){
+        new GUITest();
+     }
+}
